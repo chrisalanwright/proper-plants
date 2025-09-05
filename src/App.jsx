3a9ea368866fa0b635ef77dc37d68cd7f1cead6a
Christopher Wright <chrisalanwright@gmail.com>
2025-09-04 20:25:29 -0400

    started-jsx-files

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import { plants } from "./data";
+import PlantList from "./components/PlantList";
+import PlantDetails from "./components/PlantDetails";
+
 export default function App() {
   return <></>;
 }

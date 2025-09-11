8ac3dfcb00c5bd7af0afe3510e016bd9a7ca2c21
Christopher Wright <chrisalanwright@gmail.com>
2025-09-11 17:08:23 -0400

    created-cart-component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,3 +1,14 @@
-export default function PlantDetails({ plant }) {
-  return <div className="plant-details">{plant.name}</div>;
+export default function Cart({ cart }) {
+  return (
+    <div className="cart">
+      <h2>Cart ({cart.length})</h2>
+      {cart.map((item) => (
+        <div key={item.id}>
+          <span>{item.image}</span>
+          <span>{item.name}</span>
+          <span>Qty: {item.quantity}</span>
+        </div>
+      ))}
+    </div>
+  );
 }

68a14f880e75e665c742d66c48e6ec0eddf25fb3
Christopher Wright <chrisalanwright@gmail.com>
2025-09-11 16:42:28 -0400

    changed-app-function-and-cart.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { useState } from "react";
-import { plants } from "./data";
+import PLANTS from "./data";
 import PlantList from "./components/PlantList";
-import PlantDetails from "./components/PlantDetails";
+import Cart from "./components/Cart";
 
 export default function App() {
-  const [plants] = useState(plants);
-  const [selectedPlant, setSelectedPlant] = useState();
-  if (!selectedPlant) {
-    return <div>Please select a plant to learn more.</div>;
-  }
+  const [cart, setCart] = useState([]);
+  const addToCart = (plant) => {
+    setCart((currentCart) => {
+      const existingItem = currentCart.find((item) => item.id === plant.id);
+
+      if (existingItem) {
+        return currentCart.map((item) =>
+          item.id === plant.id ? { ...item, quantity: item.quantity + 1 } : item
+        );
+      } else {
+        return [...currentCart, { ...plant, quantity: 1 }];
+      }
+    });
+  };
   return (
     <>
-      <PlantList
-        plants={plants}
-        selectedPlant={selectedPlant}
-        setSelectedPlant={setSelectedPlant}
-      />
-      <PlantDetails plant={selectedPlant} />
+      <PlantList plants={PLANTS} addToCart={addToCart} />
+      <Cart cart={cart} />
     </>
   );
 }
---
+++ src/components/Cart.jsx
@@ -0,0 +1,3 @@
+export default function PlantDetails({ plant }) {
+  return <div className="plant-details">{plant.name}</div>;
+}
